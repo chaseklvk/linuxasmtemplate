ca6c7d7a457201a1c622900c4de256074c8f5535
Chase Zimmerman <chase@thezimmerman.net>
2018-02-28 18:34:27 -0800

    Day2 Materials

--- main.asm
+++ main.asm
@@ -4,8 +4,16 @@ bits 32
 section .data
   ;Initialized data definitions go here
   closePrompt db "Program ending, have a nice day!", 00h 
+
 section .bss
   ;Uninitialized memory reservations go here
+
+  var1  resb  1
+    .len  EQU ($-var1)          ; Size of var1
+  
+  bArray  resw  200
+    .len  EQU ($-bArray)          ; Total bytes
+    .count  EQU ($-bArray)/2      ; Total Words
 
 section .text
 
