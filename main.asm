9fbebf558673725f61f2f38c617c45511aea49a1
Chase Zimmerman <chase@thezimmerman.net>
2018-03-05 20:36:25 -0800

    Two loops that reverse string

--- main.asm
+++ main.asm
@@ -1,8 +1,9 @@
 %include "functions64.inc"
 
 section .data
-
   ;Initialized data definitions go here
+  aName db  "Abraham Lincoln", 0h
+  nameSize EQU ($-aName) - 1
 section .bss
   ;Uninitialized memory reservations go here
 
@@ -11,7 +12,30 @@ global _start
 _start:
   nop
   ; Put program code here
+  push aName
+  call PrintString
+  call Printendl
 
+  mov rcx, nameSize
+  mov rsi, 0
+  
+  L1: 
+    movzx rax, BYTE [aName + rsi]
+    push rax
+    inc rsi
+  Loop L1
+
+  mov rcx, nameSize
+  mov rsi, 0
+  L2: 
+    pop rax
+    mov [aName + rsi], al
+    inc rsi
+  Loop L2
+
+  push aName
+  call PrintString
+  call Printendl
   ; End program code here
   nop
 
