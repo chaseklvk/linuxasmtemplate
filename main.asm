40844553cd462016aabfc72d0932e357bcb3d91e
Chase Zimmerman <chase@thezimmerman.net>
2018-03-05 20:23:47 -0800

    Add 64bit functions library

--- main.asm
+++ main.asm
@@ -1,8 +1,9 @@
+%include "functions64.inc"
+
 section .data
 
   ;Initialized data definitions go here
 section .bss
-
   ;Uninitialized memory reservations go here
 
 section .text
@@ -10,11 +11,12 @@ global _start
 _start:
   nop
   ; Put program code here
-  ; End program code here
 
+  ; End program code here
   nop
 
 ; Exit the program Linux Legally!
-mov eax,1 ; Exit system call value
-mov ebx,0 ; Exit return code
-int 80h ; Call the kernel
+Exit:
+  mov rax,  60
+  mov rdi,  0
+  syscall
