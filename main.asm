1f083befbb00604b0504092fd26a3f76f86ebdfd
Chase Zimmerman <chase@thezimmerman.net>
2018-05-02 19:22:50 -0700

    init commit

--- main.asm
+++ main.asm
@@ -1,16 +1,22 @@
 %include "functions64.inc"
 
 section .data
-
   ;Initialized data definitions go here
 section .bss
   ;Uninitialized memory reservations go here
+  inputFileAddress    resq 1
+  outputFileAddress   resq 1
 
 section .text
 global _start
 _start:
   nop
   ; Put program code here
+  call Print64bitNumDecimal
+  call Printendl
+  call Print64bitNumHex
+  call Printendl
+  call Print64bitNumHex
 
   ; End program code here
   nop
